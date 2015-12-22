4d08f1b18648b894370993ccd4351b6e5bcf30d5
zwanchun <zwanchun@gmail.com>
2015-12-22 09:48:10 -0500

    Update 3sum.java

--- 3sum.java
+++ 3sum.java
@@ -10,3 +10,37 @@ The solution set must not contain duplicate triplets.
     (-1, -1, 2)
 */
 
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> rst=new ArrayList<List<Integer>>();
+        if(nums==null||nums.length<3) return rst;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0) break;
+            if(i!=0&&nums[i]==nums[i-1]) continue;
+            int left=i+1;
+            int right=nums.length-1;
+            while(left<right){
+                if(nums[i]+nums[left]>0) break;
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum==0){
+                    List<Integer> tmp=new ArrayList<Integer>();
+                    tmp.add(nums[i]);
+                    tmp.add(nums[left]);
+                    tmp.add(nums[right]);
+                    rst.add(tmp);
+                    left++;
+					right--;
+                    while(left<right&&nums[left]==nums[left-1]) left++;
+                    while(left<right&&nums[right]==nums[right+1]) right--;
+                }
+                else if(sum<0) left++;
+                else right--;
+            }
+        }
+        return rst;
+        
+        
+    }
+}
+
