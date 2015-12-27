0fc93212210c13b2c41206ba7b625678b3e05a4b
zwanchun <zwanchun@gmail.com>
2015-12-27 05:03:33 -0500

    Update validate-binary-search-tree.java

--- validate-binary-search-tree.java
+++ validate-binary-search-tree.java
@@ -12,3 +12,27 @@ Both the left and right subtrees must also be binary search trees.
 (2) 第二种方法是根据题目中的定义来实现，其实就是对于每个结点保存左右界，也就是保证结点满足它的左子树的每个结点比当前结点值小，右子树的每个结点比当前结点值大。对于根节点不用定位界，所以是无穷小到无穷大，接下来当我们往左边走时，上界就变成当前结点的值，下界不变，而往右边走时，下界则变成当前结点值，上界不变。如果在递归中遇到结点值超越了自己的上下界，则返回false，否则返回左右子树的结果。
 */
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root==null) return true;
+        return isValid(root,Long.MIN_VALUE, Long.MAX_VALUE);
+        
+    }
+    
+    private boolean isValid(TreeNode root, long min, long max){
+        if(root==null) return true;
+        if(root.val<=min||root.val>=max) return false;
+        return isValid(root.left,min,root.val)&&isValid(root.right,root.val,max);
+        
+    }
+}
+
