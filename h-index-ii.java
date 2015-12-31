30aa9c4a34486efdb4afd8aadf75ba08dfe235ac
zwanchun <zwanchun@gmail.com>
2015-12-31 10:23:57 -0500

    Update h-index-ii.java

--- h-index-ii.java
+++ h-index-ii.java
@@ -5,3 +5,17 @@ Hint:
 Expected runtime complexity is in O(log n) and the input is sorted.
 */
 
+public class Solution {
+    public int hIndex(int[] citations) {
+        if(citations==null||citations.length==0) return 0;
+        int sum=citations.length;
+        int imin=0,imax=sum-1;
+        while(imin<=imax){
+            int imid=imin+(imax-imin)/2;
+            if(sum-imid>citations[imid]) imin=imid+1;
+            else imax=imid-1;
+        }
+        return sum-imin;
+        
+    }
+}
