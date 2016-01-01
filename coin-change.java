30700154beef8dbde8a062b795d75114e8bf9b4e
zwanchun <zwanchun@gmail.com>
2016-01-01 11:18:10 -0500

    Create coin-change.java

---
+++ coin-change.java
@@ -0,0 +1,35 @@
+/*Coin Change
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+Example 1:
+coins = [1, 2, 5], amount = 11
+return 3 (11 = 5 + 5 + 1)
+
+Example 2:
+coins = [2], amount = 3
+return -1.
+
+Note:
+You may assume that you have an infinite number of each kind of coin.
+*/
+
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(coins==null||coins.length==0||amount==0) return 0;
+        int[] rst=new int[amount+1];
+        rst[0]=0;
+        for(int i=1;i<=amount;i++){
+            int num=Integer.MAX_VALUE;
+            for(int j=0;j<coins.length;j++){
+                if(i-coins[j]>=0){
+                	if(rst[i-coins[j]]!=0||i-coins[j]==0){
+                		 num=Math.min(num,rst[i-coins[j]]+1);
+                	}
+                }
+            }
+            if(num!=Integer.MAX_VALUE) rst[i]=num;
+        }
+        if(rst[amount]==0) return -1;
+        return rst[amount];  
+    }
+}
