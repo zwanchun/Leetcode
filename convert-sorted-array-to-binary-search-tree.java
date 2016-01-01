008106ae7ca662e89931caddd0df4e09c8c6a236
zwanchun <zwanchun@gmail.com>
2016-01-01 15:56:38 -0500

    Update convert-sorted-array-to-binary-search-tree.java

--- convert-sorted-array-to-binary-search-tree.java
+++ convert-sorted-array-to-binary-search-tree.java
@@ -12,3 +12,27 @@ Given an array where elements are sorted in ascending order, convert it to a hei
           root created in step 1.
 */
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums==null) return null;
+        return sortedArrayToBST(nums,0,nums.length-1);
+    }
+    
+    private TreeNode sortedArrayToBST(int[] nums, int start, int end){
+        if(start>end) return null;
+        int mid=(start+end)/2;
+        TreeNode root=new TreeNode(nums[mid]);
+        root.left=sortedArrayToBST(nums,start,mid-1);
+        root.right=sortedArrayToBST(nums,mid+1,end);
+        return root;
+    }
+}
