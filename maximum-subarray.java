6cbc0aa1a4207c2011fcfb535875f2798c89c73f
zwanchun <zwanchun@gmail.com>
2016-01-21 10:06:48 -0500

    Update maximum-subarray.java

--- maximum-subarray.java
+++ maximum-subarray.java
@@ -24,3 +24,17 @@ public class Solution {
         
     }
 }
+
+//divide and conquer
+/*solution:
+https://siddontang.gitbooks.io/leetcode-solution/content/dynamic_programming/maximum_subarray.html
+假设数组A[left, right]存在最大值区间[i, j](i>=left & j<=right)，以mid = (left + right)/2 分界，无非以下三种情况：
+
+subarray A[i,..j] is
+(1) Entirely in A[low,mid-1]
+(2) Entirely in A[mid+1,high]
+(3) Across mid
+对于(1) and (2)，直接递归求解即可，对于(3)，则需要以min为中心，向左及向右扫描求最大值，意味着在A[left, Mid]区间中找出A[i..mid], 而在A[mid+1, right]中找出A[mid+1..j]，两者加和即为(3)的解。
+*/
+
+
