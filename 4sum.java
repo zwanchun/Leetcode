b55fd221e45173f8993fd2dbd054477e8c27a8ff
zwanchun <zwanchun@gmail.com>
2017-02-27 19:20:27 -0500

    Update 4sum.java

--- 4sum.java
+++ 4sum.java
@@ -48,3 +48,53 @@ The solution set must not contain duplicate quadruplets.
         
     }
 }
+
+
+/*smallest*4*/
+public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        
+        List<List<Integer>> rst=new ArrayList<List<Integer>>();
+        if(nums==null||nums.length<4) return rst;
+        
+        Arrays.sort(nums);
+        
+        for(int i=0;i<=nums.length-4;i++){
+            if(nums[i]*4>target) break;
+            if(i!=0&&nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<=nums.length-3;j++){
+                if(nums[i]+nums[j]*3>target) break;
+                if(j!=i+1&&nums[j]==nums[j-1]) continue;
+                int left=j+1;
+                int right=nums.length-1;
+                while(left<right){
+                    if(nums[i]+nums[j]+nums[left]*2>target) break;
+                    int sum=nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum==target){
+                        List<Integer> list=new ArrayList<Integer>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[left]);
+                        list.add(nums[right]);
+                        rst.add(list);
+                        left++;
+                        right--;
+                        while(left<right&&nums[left]==nums[left-1]) left++;
+                        while(left<right&&nums[right]==nums[right+1]) right--;
+                    }
+                    else if (sum>target){
+                        right--;
+                    }
+                    else left++;
+                }
+            }
+        }
+        
+        return rst;
+        
+        
+    }
+}
+
+
+
