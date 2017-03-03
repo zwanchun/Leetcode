8028679f1652efcb44183c6b3e2349f0ba56bb5f
zwanchun <zwanchun@gmail.com>
2017-03-03 00:25:03 -0500

    Update implement-strstr.java

--- implement-strstr.java
+++ implement-strstr.java
@@ -48,3 +48,33 @@ public int strStr(String source, String target) {
         }
         return -1;
     }
+
+
+/*my solution*/
+
+class Solution {
+    /**
+     * Returns a index to the first occurrence of target in source,
+     * or -1  if target is not part of source.
+     * @param source string to be scanned.
+     * @param target string containing the sequence of characters to match.
+     */
+    public int strStr(String source, String target) {
+        // write your code here
+        if(source==null||target==null) return -1;
+        if(source.length()<target.length()) return -1;
+        if(target.length()==0) return 0;
+        
+        for(int i=0;i<=source.length()-target.length();i++){
+            if(source.charAt(i)==target.charAt(0)){
+                int j=1;
+                for(;j<target.length();j++){
+                    if(source.charAt(i+j)!=target.charAt(j)) break;
+                }
+                if(j==target.length()) return i;
+            }
+        }
+        
+        return -1;
+    }
+}
