ae26339852ec9e28151571ef733110345eb0f128
zwanchun <zwanchun@gmail.com>
2017-03-04 11:42:55 -0500

    Update longest-common-substring.java

--- longest-common-substring.java
+++ longest-common-substring.java
@@ -39,3 +39,36 @@ public class Solution {
 http://www.code123.cc/docs/leetcode-notes/string/longest_common_substring.html
 https://lefttree.gitbooks.io/leetcode/content/dynamicProgramming2/longestCommonSubstring.html
 */
+
+public class Solution {
+    /**
+     * @param A, B: Two string.
+     * @return: the length of the longest common substring.
+     */
+    public int longestCommonSubstring(String A, String B) {
+        // write your code here
+        if(A==null||B==null||A.length()==0||B.length()==0) return 0;
+        int n=A.length();
+        int m=B.length();
+        int[][] dp=new int[n+1][m+1];
+        
+        for(int i=1;i<=A.length();i++){
+            for(int j=1;j<=B.length();j++){
+               if(A.charAt(i-1)==B.charAt(j-1)){
+                   dp[i][j]=dp[i-1][j-1]+1;
+               }
+               else{
+                   dp[i][j]=0;
+               }
+            }
+        }
+        int max=0;
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                max=Math.max(max,dp[i][j]);
+            }
+        }
+        
+        return max;
+    }
+}
