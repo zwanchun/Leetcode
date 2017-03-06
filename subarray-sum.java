c8b457228217a74dcb16934b40c0241495fe6506
zwanchun <zwanchun@gmail.com>
2017-03-05 20:02:42 -0500

    Update subarray-sum.java

--- subarray-sum.java
+++ subarray-sum.java
@@ -31,3 +31,37 @@ public class Solution {
         return list;
     }
 }
+
+/*runtime:O(N)
+space:O(N)
+
+sum(i)-sum(j)=0  =>sum(i)=sum(j)
+*/
+
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers includes the index of the first number 
+     *          and the index of the last number
+     */
+    public ArrayList<Integer> subarraySum(int[] nums) {
+        // write your code here
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        if(nums==null||nums.length==0) return list; 
+        Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+        map.put(0,-1);
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(map.containsKey(sum)){
+                list.add(map.get(sum)+1);
+                list.add(i);
+                break;
+            }
+            else{
+                map.put(sum,i);
+            }
+        }
+        return list;
+    }
+}
