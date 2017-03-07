c0319681d036cdf10aa1fdc93eed393f0dfba2e8
zwanchun <zwanchun@gmail.com>
2017-03-07 18:58:35 -0500

    Update first-missing-positive.java

--- first-missing-positive.java
+++ first-missing-positive.java
@@ -27,3 +27,39 @@ public class Solution {
         return i;
     }
 }
+
+
+/*交换的条件就是3个：
+
+1: A[i] is in the range;
+2: A[i] > 0.
+3: The target is different; （如果不判断这个，会造成死循环，因为你交换过来一个一样的值）
+
+runtime:O(N)
+*/
+
+public class Solution {
+    /**    
+     * @param A: an array of integers
+     * @return: an integer
+     */
+    public int firstMissingPositive(int[] A) {
+        // write your code here
+       if(A==null||A.length==0) return 1;
+       int i=0;
+       while(i<A.length){
+           if(A[i]==i+1||A[i]<=0||A[i]>=A.length||A[i]==A[A[i]-1]) i++;
+           else if(A[i]!=i+1){
+               int tmp=A[A[i]-1];
+               A[A[i]-1]=A[i];
+               A[i]=tmp;
+           }
+       }
+       
+       for(i=0;i<A.length;i++){
+           if(A[i]!=i+1) break;
+       }
+       
+       return i+1; 
+    }
+}
