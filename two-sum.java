86c0f6afd02ecb07d0b58b9a4a261dd9b2fc0297
zwanchun <zwanchun@gmail.com>
2017-03-06 20:01:37 -0500

    Update two-sum.java

--- two-sum.java
+++ two-sum.java
@@ -29,3 +29,31 @@ public class Solution {
         
     }
 }
+
+
+public class Solution {
+    /*
+     * @param numbers : An array of Integer
+     * @param target : target = numbers[index1] + numbers[index2]
+     * @return : [index1 + 1, index2 + 1] (index1 < index2)
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        // write your code here
+        if(numbers==null||numbers.length<2) return null;
+        int[] rst=new int[2];
+        Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for(int i=0;i<numbers.length;i++){
+            if(map.containsKey(target-numbers[i])){
+                rst[0]=map.get(target-numbers[i]);
+                rst[1]=i+1;
+                break;
+            }
+            else{
+                map.put(numbers[i],i+1);
+            }
+        }
+        
+        return rst;
+        
+    }
+}
