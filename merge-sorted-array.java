365b13e9d0a26e0623b355a42da93fe028a8518c
zwanchun <zwanchun@gmail.com>
2017-03-06 19:50:46 -0500

    Update merge-sorted-array.java

--- merge-sorted-array.java
+++ merge-sorted-array.java
@@ -34,3 +34,52 @@ class Solution {
        
     }
 }
+
+
+/*O(N)
+*/
+
+class Solution {
+    /**
+     * @param A: sorted integer array A which has m elements, 
+     *           but size of A is m+n
+     * @param B: sorted integer array B which has n elements
+     * @return: void
+     */
+    public void mergeSortedArray(int[] A, int m, int[] B, int n) {
+        // write your code here
+        int i=m+n-1;
+        int indexA=m-1;
+        int indexB=n-1;
+
+        while(indexA>=0&&indexB>=0){
+            if(A[indexA]>=B[indexB]){
+                A[i]=A[indexA];
+                i--;
+                indexA--;
+            }
+            else{
+                A[i]=B[indexB];
+                i--;
+                indexB--;
+                
+            }
+        }
+        
+        while(indexA>=0){
+            A[i]=A[indexA];
+            i--;
+            indexA--;
+        }
+        
+        while(indexB>=0){
+            A[i]=B[indexB];
+            i--;
+            indexB--;
+        }
+        
+       
+      
+       
+    }
+}
