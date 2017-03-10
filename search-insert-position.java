0c2dd4ee4fd84c311f7bd41155b5b6aa191c5c0a
zwanchun <zwanchun@gmail.com>
2017-03-10 10:29:31 -0500

    Update search-insert-position.java

--- search-insert-position.java
+++ search-insert-position.java
@@ -28,3 +28,27 @@ public class Solution {
         
     }
 }
+
+
+public class Solution {
+    /** 
+     * param A : an integer sorted array
+     * param target :  an integer to be inserted
+     * return : an integer
+     */
+    public int searchInsert(int[] A, int target) {
+        // write your code here
+        if(A==null||A.length==0) return 0;
+        int start=0, end=A.length-1;
+        while(start+1<end){
+            int mid=start+(end-start)/2;
+            if(A[mid]==target) return mid;
+            else if(A[mid]<target) start=mid;
+            else end=mid;
+        }
+        
+        if(A[start]<target&&A[end]>=target) return end;
+        else if(A[start]>=target) return start;
+        else return end+1;
+    }
+}
