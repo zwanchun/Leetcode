62a785dfc4986423dd571150eaa2b91307af4a7a
zwanchun <zwanchun@gmail.com>
2017-03-11 15:26:58 -0500

    Update find-peak-element.java

--- find-peak-element.java
+++ find-peak-element.java
@@ -44,3 +44,25 @@ public class Solution {
     }
 }
 
+class Solution {
+    /**
+     * @param A: An integers array.
+     * @return: return any of peek positions.
+     */
+    public int findPeak(int[] A) {
+        // write your code here
+        if(A==null||A.length==0) return -1;
+        int start=0,end=A.length-1;
+        while(start+1<end){
+            int mid=start+(end-start)/2;
+            if(mid==0||mid==A.length-1) return -1;
+            if(A[mid]>A[mid-1]&&A[mid]>A[mid+1]) return mid;
+            else if(A[mid]<A[mid+1]) start=mid;
+            else end=mid;
+        }
+        
+        if(A[start]>A[start-1]&&A[start]>A[start+1]) return start;
+        else return end;
+    }
+}
+
