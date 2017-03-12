bab847e5f9a0f01b7eafc849ac7e67e642aef738
zwanchun <zwanchun@gmail.com>
2017-03-11 23:57:59 -0500

    Update search-in-rotated-sorted-array.java

--- search-in-rotated-sorted-array.java
+++ search-in-rotated-sorted-array.java
@@ -31,3 +31,41 @@ public class Solution {
         else return -1;
     }
 }
+
+
+public class Solution {
+    /** 
+     *@param A : an integer rotated sorted array
+     *@param target :  an integer to be searched
+     *return : an integer
+     */
+    public int search(int[] A, int target) {
+        // write your code here
+        if(A==null||A.length==0) return -1;
+        int start=0,end=A.length-1;
+        while(start+1<end){
+            int mid=start+(end-start)/2;
+            if(A[mid]==target) return mid;
+            if(A[mid]>A[start]){
+                if(target>=A[start]&&A[mid]>=target){
+                    end=mid;
+                }
+                else{
+                    start=mid;
+                }
+            }
+            else{
+                if(target>=A[mid]&&target<=A[end]){
+                    start=mid;
+                }
+                else{
+                    end=mid;
+                }
+            }
+        }
+        if(A[start]==target) return start;
+        if(A[end]==target) return end;
+        return -1;
+    }
+}
+
