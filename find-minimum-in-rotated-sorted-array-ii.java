d39a555fd07c314d57eaeeab416f5567240db919
zwanchun <zwanchun@gmail.com>
2017-03-13 23:39:53 -0400

    Update find-minimum-in-rotated-sorted-array-ii.java

--- find-minimum-in-rotated-sorted-array-ii.java
+++ find-minimum-in-rotated-sorted-array-ii.java
@@ -14,6 +14,8 @@ The array may contain duplicates.
 
 /*solutions: http://blog.csdn.net/linhuanmars/article/details/40449299*/
 
+/*由于此题输入可能有重复元素，因此在num[mid] == num[end]时无法使用二分的方法缩小start或者end的取值范围。此时只能使用递增start/递减end逐步缩小范围。*/
+
 public class Solution {
     public int findMin(int[] nums) {
        if(nums == null || nums.length==0)  return 0;  
@@ -39,4 +41,34 @@ public class Solution {
         return min;  
         
     }
+    
+    
+    
+    public class Solution {
+    /**
+     * @param num: a rotated sorted array
+     * @return: the minimum number in the array
+     */
+    public int findMin(int[] num) {
+        // write your code here
+        if(num==null||num.length==0) return -1;
+        if(num[0]<num[num.length-1]) return num[0];
+        int start=0, end=num.length-1;
+        while(start+1<end){
+            int mid=start+(end-start)/2;
+            if(num[mid]>num[end]){
+                start=mid;
+            }
+            else if(num[mid]<num[end]){
+                end=mid;
+            }
+            else{
+               end--;
+            }
+        }
+        
+        if(num[start]<num[end]) return num[start];
+        return num[end];
+    }
+}
 }
