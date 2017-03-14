a2fd825cd6e7515ba71ee7d98895b402fc6e5405
zwanchun <zwanchun@gmail.com>
2017-03-13 23:50:50 -0400

    Update search-in-rotated-sorted-array-ii.java

--- search-in-rotated-sorted-array-ii.java
+++ search-in-rotated-sorted-array-ii.java
@@ -31,3 +31,36 @@ public class Solution {
         
     }
 }
+
+/* the worst case would be O(N)
+besst: O(logN)*/
+
+public class Solution {
+    /** 
+     * param A : an integer ratated sorted array and duplicates are allowed
+     * param target :  an integer to be search
+     * return : a boolean 
+     */
+    public boolean search(int[] A, int target) {
+        // write your code here
+        if(A==null||A.length==0) return false;
+        int start=0,end=A.length-1;
+        while(start+1<end){
+            int mid=start+(end-start)/2;
+            if(A[mid]==target) return true;
+            if(A[mid]>A[end]){
+                if(target>=A[start]&&target<=A[mid]) end=mid;
+                else start=mid;
+            }
+            else if(A[mid]<A[end]){
+                if(target>=A[mid]&&target<=A[end]) start=mid;
+                else end=mid;
+            }
+            else end--;
+        }
+        
+        if(A[start]==target) return true;
+        if(A[end]==target) return true;
+        return false;
+    }
+}
