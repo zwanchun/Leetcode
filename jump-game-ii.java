bf1b499bf872e6c861a5bdb9bbcd0b0f1c7ca8e8
zwanchun <zwanchun@gmail.com>
2017-04-21 11:45:30 -0400

    Update jump-game-ii.java

--- jump-game-ii.java
+++ jump-game-ii.java
@@ -22,36 +22,28 @@ The minimum number of jumps to reach the last index is 2. (Jump 1 step from inde
 那么对于S(i)，究竟是选D = [S(i - 1) + 1, S(i - 1) + A[S(i - 1)]]这个区间的哪个值呢？ 可以让S(i)逐个取D范围的值， 并计算出 y = MAX( S(i) + A[S(i)] )，那么使得y最大的S(i)，就是局部最优解了。
 */
 
+/*runtime:O(N)*/
+
 
 public class Solution {
-    public int jump(int[] nums) {
-        if(nums==null||nums.length==0) return 0;
-        if(nums.length==1){
-            if(nums[0]>0) return 0;
-        }
-        int start=0;
-        int end=0;
-        int count=0;
-        int index=0;
-        while(end<nums.length-1){
-            start=index+1;
-            end=nums[start-1]+start-1;
-            if(end>=nums.length-1){
-                count++;
-                return count;
+    /**
+     * @param A: A list of lists of integers
+     * @return: An integer
+     */
+    public int jump(int[] A) {
+        // write your code here
+        if(A==null||A.length<=1) return 0;
+        int step=0;
+        int max_length=0;
+        int current_length=0;
+        for(int i=0;i<A.length;i++){
+            if(current_length<i){
+                step++;
+                current_length=max_length;
             }
-            int maxfar=0;
-            for(int i=start;i<=end;i++){
-                if(nums[i]+i>maxfar){
-                    maxfar=nums[i]+i;
-                    index=i;
-                }
-            }
-            count++;
-            if(end>=nums.length-1) return count;
-            end=index;
+            max_length=Math.max(max_length,A[i]+i);
         }
-        return count;
         
+        return step;
     }
 }
