a3de7f428be6c5ea09689c549df6f040bfdb7e67
zwanchun <zwanchun@gmail.com>
2017-05-16 22:00:10 -0400

    Create merge-two-sorted-lists.java

---
+++ merge-two-sorted-lists.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param ListNode l1 is the head of the linked list
+     * @param ListNode l2 is the head of the linked list
+     * @return: ListNode head of linked list
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // write your code here
+        ListNode dummy=new ListNode(0);
+        ListNode curr=dummy;
+        while(l1!=null&&l2!=null){
+            if(l1.val<l2.val){
+                curr.next=l1;
+                l1=l1.next;
+            }
+            else{
+                curr.next=l2;
+                l2=l2.next;
+            }
+            curr=curr.next;
+        }
+        
+        while(l1!=null){
+            curr.next=l1;
+            l1=l1.next;
+            curr=curr.next;
+        }
+        
+        while(l2!=null){
+            curr.next=l2;
+            l2=l2.next;
+            curr=curr.next;
+        }
+        
+        return dummy.next;
+    }
+}
